ef621d702ca530f51f02e377ec83ba6a8dbe15fd
Ibby Hadeed <ibby93@gmail.com>
2017-09-12 10:22:55 -0400

    Add chart to display overall stats

--- src/main.ts
+++ src/main.ts
@@ -1,97 +1,77 @@
 declare const Chart: any;
 
-var chartColors = {
-  red: 'rgb(255, 99, 132)',
-  orange: 'rgb(255, 159, 64)',
-  yellow: 'rgb(255, 205, 86)',
-  green: 'rgb(75, 192, 192)',
-  blue: 'rgb(54, 162, 235)',
-  purple: 'rgb(153, 102, 255)',
-  grey: 'rgb(201, 203, 207)'
-};
+interface ViewsData {
+  timestamp: string;
+  count: number;
+  uniques: number;
+  npm_downloads?: number;
+}
 
-var randomScalingFactor = function() {
-  return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100);
-};
+interface Views {
+  count: number;
+  uniques: number;
+  views: ViewsData[];
+}
 
-var config = {
-  type: 'line',
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [{
-      label: "My First dataset",
-      backgroundColor: chartColors.red,
-      borderColor: chartColors.red,
-      data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
-      ],
-      fill: false,
-    }, {
-      label: "My Second dataset",
-      fill: false,
-      backgroundColor: chartColors.blue,
-      borderColor: chartColors.blue,
-      data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
-      ],
-    }]
-  },
-  options: {
-    responsive: true,
-    title:{
-      display:true,
-      text:'Chart.js Line Chart'
-    },
-    tooltips: {
-      mode: 'index',
-      intersect: false,
-    },
-    hover: {
-      mode: 'nearest',
-      intersect: true
-    },
-    scales: {
-      xAxes: [{
-        display: true,
-        scaleLabel: {
-          display: true,
-          labelString: 'Month'
-        }
-      }],
-      yAxes: [{
-        display: true,
-        scaleLabel: {
-          display: true,
-          labelString: 'Value'
-        }
-      }]
-    }
-  }
-};
+interface Repo {
+  name: string;
+  stars: number;
+  npm_downloads: number;
+  language: string;
+  forks: number;
+  views: Views;
+}
 
+interface Stats {
+  views: number;
+  stars: number;
+  npm_downloads: number;
+  repos: Repo[];
+  mainChartData: any;
+}
 
 const onLoad = () => {
-  const canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('chart-canvas');
-  console.log('Canvas is ', canvas);
-  const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
-  let _chart = new Chart(ctx, config);
-  console.log('Created a chartss ', _chart);
 
   fetch('/assets/stats.json')
     .then((response: Response) => response.json())
-    .then((response: any) => {
+    .then((response: Stats) => {
+
+    response.mainChartData.options = {
+      responsive: true,
+      title:{
+        display:true,
+        text: 'Overall statistics'
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: false,
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: true
+      },
+      scales: {
+        xAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Month'
+          }
+        }],
+        yAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Value'
+          }
+        }]
+      }
+    };
+
+      const canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('chart-canvas');
+      const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+      let _chart = new Chart(ctx, response.mainChartData);
+
       console.log('Response is ', response);
     })
     .catch((e: any) => {
@@ -100,6 +80,6 @@ const onLoad = () => {
 
 };
 
-// document.addEventListener('DOMContentLoaded', onLoad);
+document.addEventListener('DOMContentLoaded', onLoad);
 //
 // console.log('lol');
